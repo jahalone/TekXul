8bbdc132bd6d44036a2133e17a3d3bc09e6196c5
JahAlone <lsagoegad@gmail.com>
2019-04-18 23:27:29 -1200

    first commit

---
+++ build/precache-manifest.abc6b1e2e3fdae4568d8c5e6d66de254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc57de1e01d9ca387051",
+    "url": "/static/js/main.dc57de1e.chunk.js"
+  },
+  {
+    "revision": "38d2e08c75dac70c51cd",
+    "url": "/static/js/1.38d2e08c.chunk.js"
+  },
+  {
+    "revision": "dc57de1e01d9ca387051",
+    "url": "/static/css/main.cb287557.chunk.css"
+  },
+  {
+    "revision": "38d2e08c75dac70c51cd",
+    "url": "/static/css/1.dc06a99b.chunk.css"
+  },
+  {
+    "revision": "01b7bfb58611aba53fb1c72a5a18d545",
+    "url": "/index.html"
+  }
+];
